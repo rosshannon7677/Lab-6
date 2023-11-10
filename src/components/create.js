c9f1d8db7484f5da0eb2796937a22793078edf0f
Ross Hannon <rosshannonty@gmail.com>
2023-11-10 12:33:27 +0000

    Lab6 finished

--- src/components/create.js
+++ src/components/create.js
@@ -1,11 +1,88 @@
-function Create(){
-    return(
+import { useState } from "react";
+import axios from "axios";
+
+function Create() {
+
+const [title, setTitle] = useState('');     //  Initializing our title value
+const [author, setAuthor] = useState('');   //  Initializing our Author value
+const [cover, setCover] = useState('');     //  Initializing our cover value
+const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(
+            "Title: " + title +    //  Displaying our values to console to ensure they've been taken
+            " Cover: " + cover +
+            " Author: " + author);
+
+            // Setting book to data 
+            const book = {
+                title:title,
+                cover:cover,
+                author:author
+            }
+            axios.post('http://localhost:4000/api/book', book)
+            .then()
+            .catch();
+    }
+
+    return (
         <div>
-            <h1>Create</h1>
-            <h3>Hello from create</h3>
-           
+            <h3>Hello World this is Create</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    {/* Label To prompt user to input title */}
+                    <label >Edit Book Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}  //  onChange = when this changes, e.g when title changes set the title as the value
+                    />
+                </div>
+
+                <br></br>
+                <hr></hr>
+                <br></br>
+
+                <div className="form-group">
+                    {/* Label To prompt user to input title */}
+                    <label>Input Author: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={author}
+                        onChange={(e) => { setAuthor(e.target.value) }}  //  onChange = when this changes, e.g when author changes set the author as the value
+                    />
+
+                </div>
+
+                <br></br>
+                <hr></hr>
+                <br></br>
+
+                <div className="form-group">
+
+                    {/* Label To prompt user to input title */}
+                    <label>Input Cover: </label>
+
+                    <input type="text"
+                        className="form-control"
+                        value={cover}
+                        onChange={(e) => { setCover(e.target.value) }}  //  onChange = when this changes, e.g when cover changes set the cover as the value
+                    />
+                </div>
+
+                <br></br>
+                <hr></hr>
+                <br></br>
+
+                <div>
+                    <input type="submit"    //  Button to submit form
+                        value="Add Book"
+                    ></input>
+                </div>
+            </form>
         </div>
     );
 }
+
 
 export default Create;
--- src/components/read.js
+++ src/components/read.js
@@ -1,47 +1,29 @@
+// imports
+import { useEffect, useState } from "react";
 import Books from "./books";
+import axios from "axios";
 
 function Read(){
-    
-    {/* {Implementing a JSON file } */} 
-        const data = [
-                {
-                "title": "Learn Git in a Month of Lunches",
-                "isbn": "1617292419",
-                "pageCount": 0,
-                "thumbnailUrl":
-                "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-                "status": "MEAP",
-                "authors": ["Rick Umali"],
-                "categories": []
-                },
-                {
-                "title": "MongoDB in Action, Second Edition",
-                "isbn": "1617291609",
-                "pageCount": 0,
-                "thumbnailUrl":
-                 "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-                "status": "MEAP",
-                "authors": [
-                "Kyle Banker",
-                "Peter Bakkum",
-                "Tim Hawkins",
-                "Shaun Verch",
-                "Douglas Garrett"
-                ],
-                "categories": []
-                },            
-                {
-                "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-                "isbn": "1617292036",
-                "pageCount": 0,
-                "thumbnailUrl":
-                "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-                "status": "MEAP",
-                "authors": ["Simon Holmes"],
-                "categories": []
-                }
-                ]
+ 
+    const [data, setData] = useState([]); // 
             
+    useEffect(
+        ()=>{
+            // reading read.js from the server instead of hardcoding it and reading from a link online
+            axios.get('http://localhost:4000/api/books') // Generating a request to get data
+            .then( // when data comes back we use .then to do somehting with it
+                (responce)=>{
+                    setData(responce.data.myBooks) // responce.data.books is retreving data from data.books
+                }
+            )
+            .catch(
+                (error)=>{
+                    console.log(error); // display error in console is there is one
+                }
+            )
+        }, []
+    );
+
     return(
         <div>
             <h3>Hello from read</h3>
